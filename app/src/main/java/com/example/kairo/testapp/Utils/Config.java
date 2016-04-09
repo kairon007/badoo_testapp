5173198369a4a58d16f84419d2adf086c968dc78
Amit Kairon <kairon007@gmail.com>
2016-04-09 22:43:22 +0530

    Addede youtube key

---
+++ app/src/main/java/com/example/kairo/testapp/Utils/Config.java
@@ -0,0 +1,8 @@
+package com.example.kairo.testapp.Utils;
+
+/**
+ * Created by Amit Kairon on 4/9/2016.
+ */
+public class Config {
+    String API_KEY ="AIzaSyD8RGOnDotK83C91Vo68a3MY_hX1n-L_hs";
+}
